7fc8c32b24c20f186d63eb4c523d874f1eca76e7
ElvitaKehler <mekdy.20@gmail.com>
2020-05-25 17:42:48 -0300

    Se agrega archivo product.view

---
+++ views/product.view.php
@@ -0,0 +1,30 @@
+<?php
+require_once('libs/Smarty.class.php');
+
+class ProductView {
+
+    
+
+    public function showProduct($productos,$esAdmin){
+            
+        $smarty = new Smarty();
+        $smarty->assign("base_url", BASE_URL);
+        $smarty->assign("listaProductos", $productos);
+        $smarty->assign("esadmin",$esAdmin);
+    
+        $smarty->display('showProduct.tpl');
+        
+    }
+
+    public function showProductRubros($productos){
+       // var_dump($productos);die;
+        $smarty = new Smarty();
+        $smarty->assign("base_url", BASE_URL);
+        $smarty->assign("listProductsByItem", $productos);
+
+        $smarty->display('showProductRubros.tpl');
+        
+    }
+    
+   
+}
